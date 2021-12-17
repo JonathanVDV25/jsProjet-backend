c74f702736bf40b7fb9d3f29ea51d618dd4b488e
NicolasDed <nicolas.dedoyard@student.vinci.be>
2021-12-17 22:40:26 +0100

    deleting index

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-//get homepage
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
