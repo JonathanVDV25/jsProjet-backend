2237a2c7e9eb074dbb41c5917cb4e78794c69735
NicolasDed <nicolas.dedoyard@student.vinci.be>
2021-12-17 22:47:51 +0100

    delete index

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/scores', scoresRouter);
 
