8a3bfbb78e45b0a894950bc9dbcd31bf8b015f56
laurent <laurent.vandermeersch@student.vinci.be>
2021-12-19 20:43:56 +0100

    adding source

--- model/users.js
+++ model/users.js
@@ -1,3 +1,12 @@
+/***************************************************************************************
+*    Title: <js-demos>
+*    Author: <Raphael baroni>
+*    Date: <29/10>
+*    Code version: <e19946b>
+*    Availability: <https://github.com/e-vinci/js-demos/tree/main/backend-restful-api/restful-api-essentials/step8>
+*
+***************************************************************************************/
+
 "use strict";
 const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
--- utils/session.js
+++ utils/session.js
@@ -1,3 +1,12 @@
+/***************************************************************************************
+*    Title: <js-demos>
+*    Author: <Raphael baroni>
+*    Date: <29/10>
+*    Code version: <e19946b>
+*    Availability: <https://github.com/e-vinci/js-demos/tree/main/backend-restful-api/restful-api-essentials/step8>
+*
+***************************************************************************************/
+
 const createSession = (req, username, token) => {
   req.session.username = username;
   req.session.token = token;
